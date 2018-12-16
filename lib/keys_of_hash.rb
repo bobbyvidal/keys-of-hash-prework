999c0082091ff5c8e1f97bc0d3c7132ace75d9e1
Robert Vidal <bobbyvidal@gmail.com>
2018-12-16 17:56:04 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ class Hash
     array = []
     self.each do |key, value|
       if arguments.include?(value)
-        array << value 
+        array << key 
       end
     end
     array
@@ -12,14 +12,4 @@ end
 
 
 
-class Hash
-  def keys_of(*arguments)	  
-    array = []
-    self.each do |key, value|
-      if arguments.include?(value)
-        array << key 
-      end
-    end
-    array
-  end	
-end 	
+	
